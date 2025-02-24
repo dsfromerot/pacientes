3aa7c097c9702be1b825ed6153b4180cb47b528e
dsfromerot <ds.fernandoromerot@gmail.com>
2025-02-24 13:40:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
 from flask import Flask, jsonify
-from flask_oidc import OpenIDConnect
+from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
+oauth = OAuth(app)
 
-# Configuración de Keycloak
-app.config.update({
-    'SECRET_KEY': 'secret',
-    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
-    'OIDC_ID_TOKEN_COOKIE_SECURE': False,
-    'OIDC_REQUIRE_VERIFIED_EMAIL': False,
-    'OIDC_USER_INFO_ENABLED': True,
-    'OIDC_SCOPES': ['openid', 'email', 'profile'],
-    'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
-})
-oidc = OpenIDConnect(app)
-
-# Datos hardcodeados
-pacientes = [
-    {"nombre": "Juan Perez", "edad": 30, "enfermedad": "Gripe"},
-    {"nombre": "Maria Lopez", "edad": 25, "enfermedad": "Fiebre"}
-]
+# Configura Keycloak
+oauth.register(
+    name='keycloak',
+    client_id='pacientes',
+    client_secret='EFT9lXLLTEzb9ftI59VL83V2ykza1eUW',
+    authorize_url='http://100.124.235.117:8080/auth/realms/hospital-realm/protocol/openid-connect/auth',
+    authorize_params=None,
+    access_token_url='http://100.124.235.117:8080/auth/realms/hospital-realm/protocol/openid-connect/token',
+    access_token_params=None,
+    refresh_token_url=None,
+    redirect_uri='http://100.93.128.110:5001/callback',
+    client_kwargs={'scope': 'openid profile email'},
+)
 
 @app.route('/pacientes', methods=['GET'])
-@oidc.accept_token(True)  # Protege la ruta con Keycloak
 def get_pacientes():
+    token = oauth.keycloak.authorize_access_token()
+    if not token:
+        return jsonify({"error": "Acceso no autorizado"}), 401
+    pacientes = [
+        {"nombre": "Juan Perez", "edad": 30, "enfermedad": "Gripe"},
+        {"nombre": "Maria Lopez", "edad": 25, "enfermedad": "Fiebre"}
+    ]
     return jsonify(pacientes)
 
 if __name__ == '__main__':
