753ec0bbd0d975a67fe89df1d1052fcb549665da
dsfromerot <ds.fernandoromerot@gmail.com>
2025-02-25 05:34:30 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_oidc import OpenIDConnect
 
+# Inicializar la aplicación Flask
 app = Flask(__name__)
+
+# Configuración de Flask y Keycloak
 app.config.update({
-    'SECRET_KEY': 'secret',
-    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
+    'SECRET_KEY': 'secret',  # Clave secreta para Flask
+    'OIDC_CLIENT_SECRETS': 'client_secrets.json',  # Archivo de configuración de Keycloak
     'OIDC_ID_TOKEN_COOKIE_SECURE': False,
     'OIDC_REQUIRE_VERIFIED_EMAIL': False,
     'OIDC_USER_INFO_ENABLED': True,
     'OIDC_SCOPES': ['openid', 'email', 'profile'],
     'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
 })
+
+# Inicializar la extensión Flask-OIDC
 oidc = OpenIDConnect(app)
