b7c4a6a4e21a0e73ef6960a749cb315e35809366
dsfromerot <ds.fernandoromerot@gmail.com>
2025-02-26 09:19:05 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -16,8 +16,8 @@ app.config.update({
 
 oidc = OpenIDConnect(app)
 
-# Importar rutas aquí para evitar importaciones circulares
-from app import routes
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
+
+# Importar rutas aquí para evitar importaciones circulares
+from app import routes
